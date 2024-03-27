13123d8484e74ec94b42a00c3d23ef0ffc66bba8
Alex <abelle2010@gmail.com>
2024-03-27 17:18:39 -0400

    Initial commit. Did basic enums and added project pdf and project thoughts

---
+++ PeerToPeer/src/main/java/org/example/Status.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public enum Status {
+    //2.1 Registration and de Registration
+    REGISTER,
+    REGISTERED,
+    REGISTER_DENIED,
+    DE_REGISTER,
+
+    //2.2 Publishing file related info
+    PUBLISH,
+    PUBLISHED,
+    PUBLISH_DENIED,
+    REMOVE,
+    REMOVED,
+    REMOVED_DENIED,
+
+    //2.3 Server sharing information with the registered clients
+    UPDATE,
+
+    //2.4 File transfer between clients
+    FILE_REQ,
+    FILE_CONF,
+    FILE,
+    FILE_END,
+    FILE_ERROR,
+
+    //2.5 Clients updating their contact information
+    UPDATE_CONTACT,
+    UPDATE_CONFIRMED,
+    UPDATE_DENIED
+
+
+}
