f8cbea7bb23d24229d5880424888418c2c08b6b5
Alex <abelle2010@gmail.com>
2024-03-30 10:18:14 -0400

    Added comments and refactored some code

--- PeerToPeer/src/main/java/org/coen366/ClientTwoPointOh.java
+++ PeerToPeer/src/main/java/org/coen366/ClientTwoPointOh.java
@@ -18,6 +18,7 @@ public class ClientTwoPointOh {
         int enteredPort = Integer.parseInt(getUserInput());
 
         try {
+            //This connects the user to the specified port ngl idk if it should work like this
             socket = new Socket("localhost",enteredPort);
             socket.setSoTimeout(10000);
 
@@ -35,11 +36,8 @@ public class ClientTwoPointOh {
                         System.out.println("Invalid option");
                 }
             }
-
-
-
         } catch (IOException | ClassNotFoundException e) {
-            System.out.println("The port you are trying to connect to is already taken. Try another port");
+            System.out.println("The connection has an error. Exiting...");
             exit(0);
             throw new RuntimeException(e);
         }
@@ -55,22 +53,72 @@ public class ClientTwoPointOh {
         }
     }
 
+    /**
+     * When the user chooses the option 1, they will be placed in loop of options
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private static void printRegisterOptions() throws IOException, ClassNotFoundException {
+        while (true){
+            System.out.println("Select a Register Option: ");
+            System.out.println("1.REGISTER");
+            System.out.println("2.DE_REGISTER");
+            System.out.println("3.Return");
+            String input = getUserInput();
+            switch(input) {
+                case "1":
+                    storedClient = registerWithServerTwoPointOh(ClientTwoPointOh.socket);
+                    break;
+                case "2":
+                    if(storedClient != null){
+                        deregisterWithServer(ClientTwoPointOh.socket);
+                        return;
+                    }
+                    else{
+                        System.out.println("You have not registered. Returning to main menu");
+                    }
+                break;
+                case "3":
+                    return;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+
+    }
+
+    /**
+     * This method deals with any registration the user does
+     * @param socket the current connection
+     * @return the clientInfo and stores it into a static field
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     private static ClientInfo registerWithServerTwoPointOh(Socket socket) throws IOException, ClassNotFoundException {
         System.out.println("Enter your name");
         String name = getUserInput();
 
         InetAddress clientAddress = InetAddress.getLocalHost();
 
+        //Creates a client with the entered name and with their ipaddress
         ClientInfo clientInfo = new ClientInfo(name,clientAddress);
-        Message registerMessage = new Message(Status.REGISTER,clientInfo.getRqNum(),clientInfo);
+        //Creates a message of registration
+        Message incoming = getMessage(socket, clientInfo);
+
+        System.out.println(incoming);
+        return clientInfo;
+    }
+
+    private static Message getMessage(Socket socket, ClientInfo clientInfo) throws IOException, ClassNotFoundException {
+        Message registerMessage = new Message(Status.REGISTER, clientInfo.getRqNum(), clientInfo);
 
         ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        //Sends out the register message to the server
         out.writeObject(registerMessage);
 
-        Message incoming = (Message) in.readObject();
-        System.out.println(incoming);
-        return clientInfo;
+        //This waits for the server to respond back with the confirmation
+        return (Message) in.readObject();
     }
 
     private static void deregisterWithServer(Socket socket) throws IOException, ClassNotFoundException {
@@ -94,28 +142,9 @@ public class ClientTwoPointOh {
         System.out.println("Select an Option: ");
         System.out.println("1. Register");
         System.out.println("2. Send random message");
-        System.out.println("3.Publish");
-        System.out.println("4.Update");
+        System.out.println("3. Publish");
+        System.out.println("4. Update");
     }
 
-    private static void printRegisterOptions() throws IOException, ClassNotFoundException {
 
-        while (true){
-            System.out.println("Select a Register Option: ");
-            System.out.println("1.REGISTER");
-            System.out.println("2.DE_REGISTER");
-            String input = getUserInput();
-            switch(input) {
-                case "1":
-                    storedClient = registerWithServerTwoPointOh(ClientTwoPointOh.socket);
-                    break;
-                case "2":
-                    deregisterWithServer(ClientTwoPointOh.socket);
-                    break;
-                default:
-                    System.out.println("Invalid option");
-            }
-        }
-
-    }
 }
--- PeerToPeer/src/main/java/org/coen366/ServerTwoPointOh.java
+++ PeerToPeer/src/main/java/org/coen366/ServerTwoPointOh.java
@@ -8,6 +8,8 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.System.exit;
+
 public class ServerTwoPointOh {
     private static int SERVER_PORT = 3000;
     private static int registeredClients = 0; // Initialize the variable
@@ -21,51 +23,64 @@ public class ServerTwoPointOh {
     public static void listenForUDP() {
         ServerSocket serverSocket=null;
         try {
-            // Listen for incoming UDP packets
             while(true) {
-                // Receive incoming UDP packet
+                // Listen for incoming UDP packets
                 System.out.println("Listening for client connections on server port: "+SERVER_PORT);
-
+                //This creates a new connection at the specified server port
                 serverSocket = new ServerSocket(SERVER_PORT);
-
+                // Receive incoming UDP packet
                 Socket clientSocket = serverSocket.accept();
+
                 System.out.println("Connection from the client "+clientSocket.getInetAddress().getHostAddress());
-                SERVER_PORT++;
+                SERVER_PORT++; // so if another client tries to connect, it will not crash the server
 
-                handleMessageTwoPointOh(clientSocket);
+                handleMessage(clientSocket);
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Socket is closed. Exiting");
+            exit(0);
+            throw new RuntimeException(e);
         }
-
     }
-    private static void handleMessageTwoPointOh(Socket clientSocket) throws IOException, ClassNotFoundException {
+
+    /**
+     * This method receives the message that is being sent by the client
+     * Decides what method to execute
+     * @param clientSocket is the connection the client established
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private static void handleMessage(Socket clientSocket) throws IOException, ClassNotFoundException {
         while(true){
+            //Writing out to the client
             ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            //Taking in what the client wrote
             ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+            //This reads the message that is sent from the client and deserializes it and the server waits here
             Message incoming = (Message) in.readObject();
-
             //Probably can be a case statement
-            System.out.println(incoming.getAction().toString());
+            //This statement chooses what action to do depending on the action sent
+            switch (incoming.getAction()){
+                case REGISTER :
+                        handleRegistration(incoming,out);
+                    break;
+                case DE_REGISTER :
+                    handleDeregistration(incoming,out,clientSocket);
+                    break;
+            }
 
-            if(incoming.getAction().equals(Status.REGISTER)){
-                handleRegistration(incoming,out);
-            }
-            else if(incoming.getAction().equals(Status.DE_REGISTER)){
-                clients.remove(incoming.getClientInfo());
-                registeredClients--;
-                Message outgoing = new Message(Status.DE_REGISTER,incoming.getRqNumber(),"Request granted");
-                out.writeObject(outgoing);
-                System.out.println("Closing socket with "+ clientSocket.getInetAddress().getHostAddress());
-                clientSocket.close();
-            }
         }
     }
 
+    /**
+     * This method is handles when the user is trying to Register
+     * @param incoming this is the message that is being sent from the client
+     * @param out this is the output streams so this function can send a message back to client
+     * @throws IOException
+     */
     private static void handleRegistration(Message incoming,ObjectOutputStream out) throws IOException {
-        Message outgoing = checkIfClientExsists( incoming.getClientInfo());
-        System.out.println("64");
+        Message outgoing = checkIfClientExists( incoming.getClientInfo());
         System.out.println(outgoing);
 
         if(outgoing.getAction() == Status.REGISTERED){
@@ -76,13 +91,45 @@ public class ServerTwoPointOh {
         out.writeObject(outgoing);
     }
 
-    private static Message checkIfClientExsists(ClientInfo client){
+    /**
+     * This method checks if the client exists by looping through
+     * and checking if the username exists
+     * @param client the client that is being registered
+     * @return a message to relay back to the client on their status of their Registration
+     */
+    private static Message checkIfClientExists(ClientInfo client){
+        //This prepares the outgoing message to be registered if
+        // everything goes fine in the for loop
         Message outgoing = new Message(Status.REGISTERED,client.getRqNum());
+
         for (ClientInfo c :clients) {
-            if(c.getName().equals(client.getName())){
+            if(c.getName().equalsIgnoreCase(client.getName())){
+                //This means that the user already exists, and we have to return a
+                //registration denied
                 outgoing = new Message(Status.REGISTER_DENIED,client.getRqNum(),"This username is taken");
             }
         }
         return outgoing;
+    }
+
+    /**
+     * This method handles it when the user wants to deregister
+     * @param incoming the user that is trying to leave
+     * @param out the output stream to tell the user that they checked out
+     * @param clientSocket the socket so that we can close it
+     * @throws IOException
+     */
+    private static void handleDeregistration(Message incoming,ObjectOutputStream out,Socket clientSocket) {
+        System.out.println("Dereg");
+        clients.remove(incoming.getClientInfo());
+        registeredClients--;
+        Message outgoing = new Message(Status.DE_REGISTER,incoming.getRqNumber(),"Request granted");
+        try {
+            out.writeObject(outgoing);
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Closing socket with "+ clientSocket.getInetAddress().getHostAddress());
+            //throw new RuntimeException(e);
+        }
     }
 }
